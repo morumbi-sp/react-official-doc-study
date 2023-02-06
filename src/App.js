e7ccf2f8a29f951ce1583d2b1f90c812c4dad0af
Morumbi <zun0323@gmail.com>
2023-02-06 15:16:48 -0300

    clean up for next

--- src/App.js
+++ src/App.js
@@ -1,42 +1,5 @@
-import { useState } from 'react';
-import './App.css';
-
-function Square() {
-  const [value, setValue] = useState(null);
-  const handleClick = () => {
-    setValue('X');
-  };
-  return (
-    <button className='square' onClick={handleClick}>
-      {value}
-    </button>
-  );
-}
-
-function Board() {
-  return (
-    <>
-      <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
-      </div>
-    </>
-  );
-}
-
 function App() {
-  return <Board />;
+  return <div>Hello</div>;
 }
 
 export default App;
