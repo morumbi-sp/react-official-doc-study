80393184b8466cb65244d8de6d3ea2652cc2db0d
Morumbi <zun0323@gmail.com>
2023-02-07 17:52:18 -0300

    rendering lists

--- src/App.js
+++ src/App.js
@@ -1,62 +1,20 @@
-function getImageUrl(imageId, size = 's') {
-  return 'https://i.imgur.com/' + imageId + size + '.jpg';
-}
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.',
+  ],
+};
 
-function Profile({ name, imgSrc, profession, awards, discovered }) {
+export default function App() {
   return (
-    <div>
-      <section className='profile'>
-        <h2>{name}</h2>
-        <img
-          className='avatar'
-          src={getImageUrl(imgSrc)}
-          alt={name}
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            {profession}
-          </li>
-          <li>
-            <b>Awards: {awards.length} </b>({awards.join(', ')})
-          </li>
-          <li>
-            <b>Discovered: </b>
-            {discovered}
-          </li>
-        </ul>
-      </section>
-    </div>
+    <article>
+      {poem.lines.map((line, index) => (
+        <div key={index}>
+          <p>{line}</p>
+          {index < poem.lines.length - 1 ? <hr /> : null}
+        </div>
+      ))}
+    </article>
   );
 }
-
-function App() {
-  return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile
-        name='Maria Skłodowska-Curie'
-        imgSrc='szV5sdG'
-        profession='physicist and chemist'
-        awards={[
-          'Nobel Prize in Physics',
-          'Nobel Prize in Chemistry',
-          'Davy Medal',
-          ' Matteucci Medal',
-        ]}
-        discovered='polonium (element)'
-      />
-      <Profile
-        name='Katsuko Saruhashi'
-        imgSrc='YfeOqp2'
-        profession='geochemist'
-        awards={[' Miyake Prize for geochemistry', ' Tanaka Prize']}
-        discovered='a method for measuring carbon dioxide in seawater'
-      />
-    </div>
-  );
-}
-
-export default App;
