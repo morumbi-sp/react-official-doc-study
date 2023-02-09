eba3fa5981ee1070bb981bc1bda3059522de1285
Morumbi <zun0323@gmail.com>
2023-02-09 13:51:44 -0300

    fisish Adding Interactivity

--- src/App.js
+++ src/App.js
@@ -1,45 +1,121 @@
 import { useState } from 'react';
-import { sculptureList } from './data.js';
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+function TaskList({ todos, onChangeTodo, onDeleteTodo }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <Task todo={todo} onChange={onChangeTodo} onDelete={onDeleteTodo} />
+        </li>
+      ))}
+    </ul>
+  );
+}
 
-  let hasPrev = index > 0;
-  let hasNext = index < sculptureList.length - 1;
-
-  function handleNextClick() {
-    if (hasNext) setIndex(index + 1);
+function Task({ todo, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let todoContent;
+  if (isEditing) {
+    todoContent = (
+      <>
+        <input
+          value={todo.title}
+          onChange={(e) => {
+            onChange({
+              ...todo,
+              title: e.target.value,
+            });
+          }}
+        />
+        <button onClick={() => setIsEditing(false)}>Save</button>
+      </>
+    );
+  } else {
+    todoContent = (
+      <>
+        {todo.title}
+        <button onClick={() => setIsEditing(true)}>Edit</button>
+      </>
+    );
   }
-  function handlePreviousClick() {
-    if (hasPrev) setIndex(index - 1);
-  }
+  return (
+    <label>
+      <input
+        type='checkbox'
+        checked={todo.done}
+        onChange={(e) => {
+          onChange({
+            ...todo,
+            done: e.target.checked,
+          });
+        }}
+      />
+      {todoContent}
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </label>
+  );
+}
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
-
-  let sculpture = sculptureList[index];
+function AddTodo({ onAddTodo }) {
+  const [title, setTitle] = useState('');
   return (
     <>
-      <button onClick={handlePreviousClick} disabled={!hasPrev}>
-        Previous
+      <input
+        placeholder='Add todo'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          setTitle('');
+          onAddTodo(title);
+        }}
+      >
+        Add
       </button>
-      <button onClick={handleNextClick} disabled={!hasNext}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? 'Hide' : 'Show'} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
+  );
+}
+
+let nextId = 3;
+const initialTodos = [
+  { id: 0, title: 'Buy milk', done: true },
+  { id: 1, title: 'Eat tacos', done: false },
+  { id: 2, title: 'Brew tea', done: false },
+];
+
+export default function TaskApp() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  function handleAddTodo(title) {
+    const newTodos = [...todos, { id: nextId++, title, done: false }];
+    setTodos(newTodos);
+  }
+
+  function handleChangeTodo(nextTodo) {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === nextTodo.id) {
+        return { ...todo, title: nextTodo.title, done: nextTodo.done };
+      } else {
+        return todo;
+      }
+    });
+    setTodos(newTodos);
+  }
+
+  function handleDeleteTodo(todoId) {
+    const newTodos = todos.filter((todo) => todo.id !== todoId);
+    setTodos(newTodos);
+  }
+
+  return (
+    <>
+      <AddTodo onAddTodo={handleAddTodo} />
+      <TaskList
+        todos={todos}
+        onChangeTodo={handleChangeTodo}
+        onDeleteTodo={handleDeleteTodo}
+      />
     </>
   );
 }
